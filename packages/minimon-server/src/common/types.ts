9b2cc6d8c28d300b049d66fa3bfbf2fed4dcc6a9
Kasim Ahmic <kasim.ahmic@gmail.com>
2022-07-30 22:58:09 -0400

    refactor(server): Remove module augmentation since systeminformation has been updated

--- packages/minimon-server/src/common/types.ts
+++
@@ -1,9 +0,0 @@
-import 'systeminformation';
-
-// The powerShellStart and powerShellRelease functions aren't exported from systeminformation
-// so we have to augment the module.
-// See https://github.com/sebhildebrandt/systeminformation/pull/700
-declare module 'systeminformation' {
-  export function powerShellStart(): void;
-  export function powerShellRelease(): void;
-}
