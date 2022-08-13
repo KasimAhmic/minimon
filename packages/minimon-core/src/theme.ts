c61833cd113857def6c9721c0c66954fb1b439c0
Kasim Ahmic <kasim.ahmic@gmail.com>
2022-08-13 19:37:50 -0400

    feat(core): Add theme types

---
+++ packages/minimon-core/src/theme.ts
@@ -0,0 +1,14 @@
+interface Colors {
+  backgroundColor: string;
+  dialColor: string;
+  dialLabelFontColor: string;
+  dialLabelFontSize: string;
+  dialLabelFontWeight: string;
+  dialValueFontColor: string;
+  dialValueFontSize: string;
+  dialValueFontWeight: string;
+}
+
+export interface MinimonTheme {
+  colors: Colors;
+}
