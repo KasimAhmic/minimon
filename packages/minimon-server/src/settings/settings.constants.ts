276aa8bb340c2b69fb5dbdda4c015a806cc3eb85
Kasim Ahmic <kasim.ahmic@gmail.com>
2022-08-20 11:04:59 -0400

    feat(server): Add schema validation for settings.json

--- packages/minimon-server/src/settings/settings.constants.ts
+++ packages/minimon-server/src/settings/settings.constants.ts
@@ -1,2 +1,30 @@
+import { Layout, Settings, ThemeMode, Widget } from '@ahmic/minimon-core';
+import Joi from 'joi';
+
 export const INTERVAL_UPDATED = 'settings.intervalUpdated';
 export const SETTINGS_FILE = 'SETTINGS_FILE';
+
+export const settingsValidationSchema = Joi.object<Settings, true>({
+  clickToReload: Joi.boolean(),
+  showDebugScreen: Joi.boolean(),
+  themeMode: Joi.string().valid(...Object.values(ThemeMode)),
+  pollingInterval: Joi.number().greater(300),
+  // A typing bug in Joi is preventing us from using nested Joi.StrictSchemaMap types.
+  //
+  // Issue: https://github.com/sideway/joi/issues/2764
+  // PR:    https://github.com/sideway/joi/pull/2627
+  //
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  layout: Joi.object<Layout, true>({
+    columns: Joi.number().greater(0),
+    rows: Joi.number().greater(0),
+    widgets: Joi.array().items(
+      Joi.object<Widget, true>({
+        label: Joi.string(),
+        vital: Joi.string(),
+        property: Joi.string(),
+      }),
+    ),
+  }),
+});
