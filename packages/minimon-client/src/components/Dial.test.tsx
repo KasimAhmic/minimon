1b6ed88f76631555ec106ade56cddd0f7abf4bc9
Kasim Ahmic <kasim.ahmic@gmail.com>
2022-10-08 12:45:51 -0400

    test(client): Fix Dial unit test

--- packages/minimon-client/src/components/Dial.test.tsx
+++ packages/minimon-client/src/components/Dial.test.tsx
@@ -7,9 +7,15 @@ const mockUseRescaledValue = jest.fn();
 jest.mock('hooks', () => ({
   ...jest.requireActual('hooks'),
   useRescaledValue: () => mockUseRescaledValue(),
+  useSettingsSelector: (cb: Function) => jest.fn((cb) => cb({ layout: { rows: 2, columns: 3 } })),
 }));
 
 describe('Dial', () => {
+  beforeAll(() => {
+    window.innerWidth = 1920;
+    window.innerHeight = 1080;
+  });
+
   it('renders without crashing', () => {
     mockUseRescaledValue.mockReturnValue({ rescaledValue: 75, valueLabel: '75%' });
 
